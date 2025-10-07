cac3a22028cfd2bff27d58ca2c2fa434ef466fdf
あずきしろもち <azukisiromochi@gmail.com>
2025-10-07 18:03:04 +0900

    💄 shadcnを導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  imports: {
+    dirs: ['composables', 'composables/**', 'types', 'types/**'],
+  },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
 })
