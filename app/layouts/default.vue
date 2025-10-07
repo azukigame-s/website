4079a5d74c50c92f6601f2a2b9567e195135cf8f
あずきしろもち <azukisiromochi@gmail.com>
2025-10-07 19:00:30 +0900

    🎨 デフォルトレイアウトを用意

---
+++ app/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+    <p>Some default layout content shared across all pages</p>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+///// Header
+useHead({
+  title: 'キミノコエ',
+})
+</script>
+
+<style>
+:root {
+  --kiminokoe-main-color: #000000;
+  --kiminokoe-max-width: 1080px;
+}
+
+.main {
+  max-width: var(--kiminokoe-max-width);
+  margin: auto;
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <Button>Click here!</Button>
+</template>
+
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
